1d858bf682622870799a4642ebb5411871eb55df
william-wheatleyuhl <william.wheatleyuhl@pcc.edu>
2018-02-13 22:20:39 -0800

    Add method to say hello by name

--- HelloWorld.java
+++ HelloWorld.java
@@ -14,4 +14,13 @@ public class HelloWorld
     {
         System.out.println("Hello, World!");
     }
+    
+    /**
+     * Say hello to someone by name
+     * @param name
+     */
+    public void sayHello(String name)
+    {
+        System.out.println("Hello, " + name + "!");
+    }
 }
