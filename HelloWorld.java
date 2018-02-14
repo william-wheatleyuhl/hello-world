a7ca2a0a9636698f70007b44ef725c832b7a304c
william-wheatleyuhl <william.wheatleyuhl@pcc.edu>
2018-02-13 22:18:40 -0800

    Created HelloWorld Class

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+
+/**
+ * Say Hello
+ *
+ * @author William Wheatley-Uhl
+ * @version 2018.02.13
+ */
+public class HelloWorld
+{
+    /**
+     * Say hello to the world
+     */
+    public void sayHello()
+    {
+        System.out.println("Hello, World!");
+    }
+}
