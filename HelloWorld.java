320578a2b43f7d63388a0b3646c58d1a3c26aaa0
william-wheatleyuhl <william.wheatleyuhl@pcc.edu>
2018-02-16 15:20:57 -0800

    Added Custom 'Favorite Fruit' message method

--- HelloWorld.java
+++ HelloWorld.java
@@ -23,4 +23,21 @@ public class HelloWorld
     {
         System.out.println("Hello, " + name + "!");
     }
+    
+    /**
+     * Ask for Name, and Favorite Fruit, and display a message
+     * Adjust the message if plural fruits are given
+     * @param name
+     * @param fruit
+     */
+    public void favoriteFruit(String name, String fruit)
+    {
+        int length = fruit.length();
+        if(fruit.lastIndexOf("s") > 0 && fruit.lastIndexOf("s") == length - 1) {
+            System.out.println("Hey, " + name + "-kun! Did you know the best " + fruit + " are grown in Kantaro?");
+        }
+        else if(fruit.lastIndexOf("s") != length -1) {
+            System.out.println("Hey, " + name + "-kun! Did you know the best " + fruit + "s are grown in Kantaro?");
+        }
+    }
 }
